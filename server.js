2d9d3b90f8c9572cbee6c8ae3acc191b158ffc3a
Sam Armstrong <armstrong.samuelm@gmail.com>
2018-09-11 23:08:24 +1200

    Removed localhost from ACAO
    Only one value allowed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ db.once('open', () => console.log('MongoDB Connected'));
 // Serve static files from the React App
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, http://etstooltracker.s3-website-ap-southeast-2.amazonaws.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://etstooltracker.s3-website-ap-southeast-2.amazonaws.com/');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
